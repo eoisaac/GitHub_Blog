55de39e4cd7358b83e35fbcc56f111c95e01ebe9
Isaac Santiago <eoisaacc@gmail.com>
2022-10-22 16:29:33 -0300

    :package: build: configure project setup

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import './styles/main.css'
+
+export const App = () => {
+  return <h1 className="text-3xl font-bold bg-primary">Hello world!</h1>
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        'base-title': 'rgb(var(--color-base-title) / <alpha-value>)',
+        'base-subtitle': 'rgb(var(--color-base-subtitle) / <alpha-value>)',
+        'base-text': 'rgb(var(--color-base-text) / <alpha-value>)',
+        'base-span': 'rgb(var(--color-base-span) / <alpha-value>)',
+        'base-label': 'rgb(var(--color-base-label) / <alpha-value>)',
+        'base-border': 'rgb(var(--color-base-border) / <alpha-value>)',
+        'base-post': 'rgb(var(--color-base-post) / <alpha-value>)',
+        'base-profile': 'rgb(var(--color-base-profile) / <alpha-value>)',
+        'base-background': 'rgb(var(--color-base-background) / <alpha-value>)',
+        'base-input': 'rgb(var(--color-base-input) / <alpha-value>)',
+      },
+    },
+  },
+  plugins: [],
+}
