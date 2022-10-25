57409f1467435ce9aee8630b025374301e3d7faf
Isaac Santiago <eoisaacc@gmail.com>
2022-10-24 23:36:46 -0300

    :bug: fix: code block color theme

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,5 +1,7 @@
+import { useContext } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
-import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { vs2015, vs } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { ThemeContext } from '../contexts/ThemeContext'
 interface CodeBlockProps {
   children: string
   language: string
@@ -7,9 +9,13 @@ interface CodeBlockProps {
 
 export const CodeBlock = ({ children, language }: CodeBlockProps) => {
   // atomOneDark
+
+  const { isDark } = useContext(ThemeContext)
+
+  const theme = isDark ? vs2015 : vs
   return (
     <SyntaxHighlighter
-      style={vs2015}
+      style={theme}
       language={language}
       showLineNumbers={true}
       showInlineLineNumbers={false}
